6ef6f38ddf0f6c0e8eada51ff0f9a0228c77f297
Latra Gallucci <personadelmonton@gmail.com>
2024-08-20 09:00:29 +0200

    update awards

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -8,30 +8,31 @@ function Awards() {
   return (
     <section id="awards">
       <h2>Awards</h2>
-      <div className="entry">
-        <div className="entry-content">
-            <h3 className="entry-title">HackUPC 2023</h3>
-            <h3 className="entry-subtitle">Vueling Party Appventure</h3>
+      {awardsData.map((award, index) => (
+        <div className="entry" key={index}>
+          <div className="entry-content">
+            <h3 className="entry-title">{award.event} - {award.challenge}</h3>
+            <h3 className="entry-subtitle">{award.name}</h3>
             <div  className="entry-content">
               <p className="entry-description">
-                A multiplatform videogame app created using unity and connected with a LAN server for enhance the flight experience.
+                {award.description}
               </p>
               <p className="entry-description">
-                <b>Project:</b> <a href='https://devpost.com/software/vueling-party-appventure'  target="_blank" rel="noopener noreferrer">devpost.com/software/vueling-party-appventure</a>
+              {award.link && (
+              <p className="entry-project-link">
+                <b>Project:</b> <a href={award.link} target="_blank" rel="noopener noreferrer">
+                  {award.link.replace(/^https?:\/\//, '')}
+                </a>
               </p>
-
+            )}
+              </p>
             </div>
-
           </div>
-          
           <div className="project-url">
-            May 2023<br></br><br></br>
-            Vueling Challenge 1st Place <i class="fa-solid fa-trophy"></i><br></br>
-            HackUPC 2nd Place <i class="fa-solid fa-trophy"></i>
-
+            {award.date}<br></br><br></br>
           </div>
         </div>
-     
+      ))}
     </section>
   );
 }
